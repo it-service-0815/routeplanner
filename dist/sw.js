920b25094f50bca9085d37ded32eeca84d03394d
it-service-0815 <68535960+it-service-0815@users.noreply.github.com>
2026-07-27 18:15:43 +0200

    R4.2/R4.3: update dist/sw.js

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE='routenplaner-r4-1-20260727';
-const ASSETS=['./','./index.html','./styles.css?v=6','./app.js?v=6','./core.js','./routing.js','./manifest.webmanifest'];
+const CACHE='routenplaner-r4-3-20260727';
+const ASSETS=['./','./index.html','./styles.css?v=7','./app.js?v=7','./core.js','./routing.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
